674109021916d30a62657339044900f286b6d4df
pallab <pallab38@gmail.com>
2023-06-01 15:08:19 +0200

    formatted code

--- model/unet.py
+++ model/unet.py
@@ -1,69 +1,83 @@
 #### https://github.com/Pallab38/cvgLab_anr/blob/main/cvgLab_anr/unet.py
-import torch 
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-def down(in_ch, out_ch, with_bn=True):
-    layers=[
-        nn.Conv2d(in_ch, out_ch, kernel_size=3, 
-                  padding=1, stride=2, bias = not with_bn),
-        nn.ReLU(inplace=True),        
-    ]
-    if with_bn:
-        layers.append(nn.BatchNorm2d(out_ch))
-    else: 
-        layers.append(nn.GroupNorm(num_groups=8, num_channels=out_ch)) ### num_channels should be divided by num_groups
-    layers.append(nn.Conv2d(out_ch, out_ch, kernel_size=1)) ##padding=0, stride=1
-    layers.append(nn.ReLU(inplace=True))
-    
-    return nn.Sequential(*layers)
 
-def middle(in_ch, out_ch, with_bn=True):
+def down(in_ch, out_ch, with_bn=True):
     layers = [
-        nn.Conv2d(in_ch, out_ch, kernel_size=1), ##padding=0, stride=1
-        nn.ReLU(inplace=True)
+        nn.Conv2d(in_ch, out_ch, kernel_size=3, padding=1, stride=2, bias=not with_bn),
+        nn.ReLU(inplace=True),
     ]
     if with_bn:
         layers.append(nn.BatchNorm2d(out_ch))
     else:
-        layers.append(nn.GroupNorm(num_groups=8, num_channels=out_ch)) ### num_channels should be divided by num_groups
-    layers.append(nn.Conv2d(out_ch, out_ch, kernel_size=1))  ##padding=0, stride=1 
+        layers.append(
+            nn.GroupNorm(num_groups=8, num_channels=out_ch)
+        )  ### num_channels should be divided by num_groups
+    layers.append(nn.Conv2d(out_ch, out_ch, kernel_size=1))  ##padding=0, stride=1
     layers.append(nn.ReLU(inplace=True))
-    
+
     return nn.Sequential(*layers)
+
+
+def middle(in_ch, out_ch, with_bn=True):
+    layers = [
+        nn.Conv2d(in_ch, out_ch, kernel_size=1),  ##padding=0, stride=1
+        nn.ReLU(inplace=True),
+    ]
+    if with_bn:
+        layers.append(nn.BatchNorm2d(out_ch))
+    else:
+        layers.append(
+            nn.GroupNorm(num_groups=8, num_channels=out_ch)
+        )  ### num_channels should be divided by num_groups
+    layers.append(nn.Conv2d(out_ch, out_ch, kernel_size=1))  ##padding=0, stride=1
+    layers.append(nn.ReLU(inplace=True))
+
+    return nn.Sequential(*layers)
+
 
 def up(in_ch, out_ch, with_bn=True):
     layers = [
         nn.ConvTranspose2d(in_ch, out_ch, kernel_size=3, padding=0, stride=2),
-        nn.ReLU(inplace=True)
+        nn.ReLU(inplace=True),
     ]
     if with_bn:
         layers.append(nn.BatchNorm2d(out_ch))
     else:
-        layers.append(nn.GroupNorm(num_groups=8, num_channels= out_ch))
-    layers.append(nn.Conv2d(out_ch, out_ch, kernel_size=2)) ## padding=0, stride=1
+        layers.append(nn.GroupNorm(num_groups=8, num_channels=out_ch))
+    layers.append(nn.Conv2d(out_ch, out_ch, kernel_size=2))  ## padding=0, stride=1
     layers.append(nn.ReLU(inplace=True))
-    
+
     return nn.Sequential(*layers)
 
 
 class UNet(nn.Module):
-    def __init__(self,in_channels, out_channels, 
-                 mid_channels=256,  with_bn=True):
-        super(UNet,self).__init__()
-        
-        self.down1 = down(in_channels, mid_channels, with_bn) ##
-        self.down2 = down(mid_channels, 2*mid_channels, with_bn)
-        
-        self.mid1 = middle(2*mid_channels, 8*mid_channels, with_bn)
-        self.mid2 = middle(8*mid_channels+2*mid_channels, 2*mid_channels, with_bn )##cat[m1,d1]
-        
-        self.up1 = up(2*mid_channels+ 2* mid_channels, mid_channels, with_bn)##cat[m2,d2]
-        self.up2 = up(mid_channels+mid_channels, mid_channels//2) ##cat[u1,d1]
-        
-        self.out = nn.Conv2d(in_channels=mid_channels//2, out_channels=out_channels,
-                             kernel_size=1, padding=0, stride=1)   
-        
+    def __init__(self, in_channels, out_channels, mid_channels=256, with_bn=True):
+        super(UNet, self).__init__()
+
+        self.down1 = down(in_channels, mid_channels, with_bn)  ##
+        self.down2 = down(mid_channels, 2 * mid_channels, with_bn)
+
+        self.mid1 = middle(2 * mid_channels, 8 * mid_channels, with_bn)
+        self.mid2 = middle(
+            8 * mid_channels + 2 * mid_channels, 2 * mid_channels, with_bn
+        )  ##cat[m1,d1]
+
+        self.up1 = up(
+            2 * mid_channels + 2 * mid_channels, mid_channels, with_bn
+        )  ##cat[m2,d2]
+        self.up2 = up(mid_channels + mid_channels, mid_channels // 2)  ##cat[u1,d1]
+
+        self.out = nn.Conv2d(
+            in_channels=mid_channels // 2,
+            out_channels=out_channels,
+            kernel_size=1,
+            padding=0,
+            stride=1,
+        )
+
     def forward(self, x):
         d1 = self.down1(x)
         # print(f"d1:{d1.shape}")
@@ -71,28 +85,27 @@ class UNet(nn.Module):
         # print(f"d2:{d2.shape}")
         m1 = self.mid1(d2)
         # print(f"m1:{m1.shape}")
-        m2 = self.mid2(torch.cat([m1,d2], dim=1)) 
+        m2 = self.mid2(torch.cat([m1, d2], dim=1))
         # print(f"m1:{m1.shape}, cat[m1,d2]:{torch.cat([m1,d2],dim=1).shape}")
-        # print(f"m2:{m2.shape}, cat[m2,d2]:{torch.cat([m2,d2], dim=1).shape}")    
-        u1 = self.up1(torch.cat([m2,d2], dim=1))
+        # print(f"m2:{m2.shape}, cat[m2,d2]:{torch.cat([m2,d2], dim=1).shape}")
+        u1 = self.up1(torch.cat([m2, d2], dim=1))
         # print(f"u1:{u1.shape}")
-        u2 = self.up2(torch.cat([u1,d1], dim=1))
+        u2 = self.up2(torch.cat([u1, d1], dim=1))
         out = self.out(u2)
         # print(f"u1:{u1.shape}, u2:{u2.shape}")
         # print(f"out.shape:{out.shape}")
-        
+
         # img = torch.tanh(out[:,1:4,:,:])
         # mask = torch.sigmoid(out[:,0,:,:])
         # return img, mask
-        
-        return out
-        
 
-if __name__=="__main__":
-    x = torch.ones(4,32,1024,1024) #### [BxCxHxW]
+        return out
+
+
+if __name__ == "__main__":
+    x = torch.ones(4, 32, 1024, 1024)  #### [BxCxHxW]
     print(f"x:{x.shape}")
     # my_unet = UNet(in_channels=32, out_channels=4)
     # # print(f"my_unet Modules:{list(my_unet.modules())}")
     # img, mask = my_unet(x)
     # print(f"img:{img.shape}, mask:{mask.shape}")
-    
--- model/utilities/texturePoints.py
+++ model/utilities/texturePoints.py
@@ -1,21 +1,40 @@
-import os 
+import os
 from os.path import join, isfile, isdir
 from pathlib import Path
 
 import numpy as np
 from numpy.random import default_rng
-import torch 
+import torch
 import trimesh
 
 from typing import List
 from utilities.nara_processing_surface_new import TextureProjector3d
 from utilities.FourierFeature import input_mapping
 
-pid_dict={0:"313",1:"315",2:"377",3:"386",4:"387",5:"390",6:"392",7:"393",8:"394"}
+pid_dict = {
+    0: "313",
+    1: "315",
+    2: "377",
+    3: "386",
+    4: "387",
+    5: "390",
+    6: "392",
+    7: "393",
+    8: "394",
+}
+
 
 class TexturePoints3D:
-    def __init__(self, person_ids:List[str], mesh_num:int, n_sample:int, current_epoch:int,
-                 save_path:str, device:torch.device, data_root: str="/home/user/my_zjuMocap") -> None:
+    def __init__(
+        self,
+        person_ids: List[str],
+        mesh_num: int,
+        n_sample: int,
+        current_epoch: int,
+        save_path: str,
+        device: torch.device,
+        data_root: str = "/home/user/my_zjuMocap",
+    ) -> None:
         """Learns The 3D Texture Points (u x h x w).
         **Texture Points** are w/o batch.
 
@@ -27,7 +46,7 @@ class TexturePoints3D:
             save_path (str): Where to save Learned 3D textures
             device (torch.device): Cuda or CPU
             data_root (str, optional): Directory for Mesh, UV Table. Defaults to "/home/user/my_zjuMocap".
-        Returns: 
+        Returns:
 
         """
         super().__init__()
@@ -36,57 +55,69 @@ class TexturePoints3D:
         self.n_sample = n_sample
         self.current_epoch = current_epoch
         self.device = device
-        self.save_dir = join(save_path,"learned_texPts")
+        self.save_dir = join(save_path, "learned_texPts")
         self.mesh_dir = join(data_root, "50_meshes")
         self.uv_fpath = join(data_root, "uv_table.npy")
         self.data = dict()
         if not isdir(self.save_dir):
-            assert (current_epoch== -1), f"Requires Learned Texture Points for {self.current_epoch} at {self.save_dir}"
+            assert (
+                current_epoch == -1
+            ), f"Requires Learned Texture Points for {self.current_epoch} at {self.save_dir}"
             Path(self.save_dir).mkdir(parents=True, exist_ok=True)
-        
-        
-        
-        if(self.current_epoch==-1):
+
+        if self.current_epoch == -1:
             ###############################################################
             ########    Create Initial Random 3d Texture Points    ########
             ###############################################################
             torch.manual_seed(1310)
             self.rng = default_rng(12345)
-            base_tex3d = torch.FloatTensor(self.n_sample, 3).uniform_() ## torch.float32, [300000, 3])
+            base_tex3d = torch.FloatTensor(
+                self.n_sample, 3
+            ).uniform_()  ## torch.float32, [300000, 3])
             # print(f"base_tex:{type(base_tex3d), base_tex3d.dtype, base_tex3d.shape}")
             # print(f"base_tex: {torch.min(base_tex3d).item(),torch.max(base_tex3d).item()}")
             person_id = self.person_ids[0]
-            mesh_fpath = join(self.mesh_dir, person_id, "%06d.obj"%self.mesh_num)         
+            mesh_fpath = join(self.mesh_dir, person_id, "%06d.obj" % self.mesh_num)
             my_mesh = trimesh.load(mesh_fpath)
             V = my_mesh.vertices
             F = my_mesh.faces
             T = np.load(self.uv_fpath)
-            
-            proj = TextureProjector3d(T,F=F)
-            pts3d, pts_uv, normal_noise, noramls = proj.random_sample(n_sample,V) ### (300000, 2) 0.01966 0.9945
+
+            proj = TextureProjector3d(T, F=F)
+            pts3d, pts_uv, normal_noise, noramls = proj.random_sample(
+                n_sample, V
+            )  ### (300000, 2) 0.01966 0.9945
             ### Convert Value Range: [0,1]->[-1,1]
-            pts_uv = 2.*(pts_uv - np.min(pts_uv))/np.ptp(pts_uv) - 1 ## (300000, 2), -1.0, 1.0)            
-            noise_fixed = self.rng.uniform(-0.1, 0.1, (self.n_sample, 1) ) ## (300000, 1)
-            uvw = np.concatenate((pts_uv, noise_fixed), axis=1) ## (300000, 3)
-            base_tex3d = torch.from_numpy(uvw.copy()).float() ## torch.float32, [300000, 3])
+            pts_uv = (
+                2.0 * (pts_uv - np.min(pts_uv)) / np.ptp(pts_uv) - 1
+            )  ## (300000, 2), -1.0, 1.0)
+            noise_fixed = self.rng.uniform(
+                -0.1, 0.1, (self.n_sample, 1)
+            )  ## (300000, 1)
+            uvw = np.concatenate((pts_uv, noise_fixed), axis=1)  ## (300000, 3)
+            base_tex3d = torch.from_numpy(
+                uvw.copy()
+            ).float()  ## torch.float32, [300000, 3])
             # print(f"base_tex:{type(base_tex3d), base_tex3d.dtype, base_tex3d.shape}")
-            
+
             # print(f"base_tex: {torch.min(base_tex3d).item(),torch.max(base_tex3d).item()}")
-            base_fpath = join(self.save_dir,"base.pth")
+            base_fpath = join(self.save_dir, "base.pth")
             torch.save(base_tex3d, base_fpath)
-            
-            for pid in self.person_ids:                
-                ##### Save the Texture Points ####              
+
+            for pid in self.person_ids:
+                ##### Save the Texture Points ####
                 tex_pts = torch.load(base_fpath).to(self.device)
                 tex_pts.requires_grad = True
-                optim = torch.optim.Adam([tex_pts], lr=0.05, amsgrad=True, weight_decay=0)
-                self.data[pid] = {"tex_pts": tex_pts, "optim":optim}
+                optim = torch.optim.Adam(
+                    [tex_pts], lr=0.05, amsgrad=True, weight_decay=0
+                )
+                self.data[pid] = {"tex_pts": tex_pts, "optim": optim}
 
         else:
             ###############################################################
             ########       Load Learned 3d Texture Points          ########
-            ###############################################################              
-            path = join(self.save_dir, "ep%06d"%current_epoch)
+            ###############################################################
+            path = join(self.save_dir, "ep%06d" % current_epoch)
             for pid in self.person_ids:
                 tex_fpath = join(path, f"texture_{pid}.pth")
                 optim_fpath = join(path, f"texture_{pid}_optim.pth")
@@ -94,19 +125,19 @@ class TexturePoints3D:
                 assert isfile(optim_fpath), optim_fpath
                 tex_pts = torch.load(tex_fpath).to(self.device)
                 tex_pts.requires_grad = True
-                optim = torch.optim.Adam([tex_pts], lr=0.005, 
-                                         amsgrad=True, weight_decay=0)
+                optim = torch.optim.Adam(
+                    [tex_pts], lr=0.005, amsgrad=True, weight_decay=0
+                )
                 optim_checkpoint = torch.load(optim_fpath)
                 optim.load_state_dict(optim_checkpoint["optim"])
                 self.data[pid] = {"tex_pts": tex_pts, "optim": optim}
-    
-    
+
     def save_learned_texPts(self, epoch=-1):
-        if (epoch > -1):
-            epoch_dir = join(self.save_dir, "ep%06d"%epoch)
+        if epoch > -1:
+            epoch_dir = join(self.save_dir, "ep%06d" % epoch)
             assert not isdir(epoch_dir), epoch_dir
             Path(epoch_dir).mkdir(parents=True, exist_ok=True)
-        
+
         for pid in self.data.keys():
             data = self.data[pid]
             tex_pts = data["tex_pts"]
@@ -116,19 +147,18 @@ class TexturePoints3D:
             torch.save({"optim": optim.state_dict()}, optim_fpath)
             torch.save(tex_pts, tex_fpath)
             # print("tex_pts is saved in :", tex_fpath)
-            
-            if(epoch > -1):
+
+            if epoch > -1:
                 f_path = join(epoch_dir, f"texture_{pid}.pth")
                 optim_path = join(epoch_dir, f"texture_{pid}_optim.pth")
-                torch.save({"optim":optim.state_dict()}, optim_path)
+                torch.save({"optim": optim.state_dict()}, optim_path)
                 torch.save(tex_pts, f_path)
-    
-    
+
     def get_texPts_for_evaluation(self, person_ids: List[int]):
-        texPts_3d = []    
+        texPts_3d = []
         for pid in person_ids:
             data = self.data[pid]
-            tex_pts = data["tex_pts"]            
+            tex_pts = data["tex_pts"]
             texPts_3d.append(tex_pts.unsqueeze(0))
         texPts_3d = torch.cat(texPts_3d, dim=0)
         return texPts_3d
@@ -150,18 +180,25 @@ class TexturePoints3D:
             # print(f"{type(tex_pts), tex_pts.requires_grad}")
             optim = data["optim"]
             texPts_3d.append(tex_pts.unsqueeze(0))
-            
+
             if pid not in already_used_pids:
                 Optim.append(optim)
                 already_used_pids.add(pid)
         texPts_3d = torch.cat(texPts_3d, dim=0)
         return texPts_3d, Optim
-            
-        
+
 
 class TexturePoints:
-    def __init__(self, person_ids:List[str], mesh_num:int, n_sample:int, current_epoch:int,
-                 save_path:str, device:torch.device, data_root: str="/home/user/my_zjuMocap") -> None:
+    def __init__(
+        self,
+        person_ids: List[str],
+        mesh_num: int,
+        n_sample: int,
+        current_epoch: int,
+        save_path: str,
+        device: torch.device,
+        data_root: str = "/home/user/my_zjuMocap",
+    ) -> None:
         """Learns The 3D Texture Points (u x h x w).
         **Texture Points** are w/o batch.
 
@@ -173,7 +210,7 @@ class TexturePoints:
             save_path (str): Where to save Learned 3D textures
             device (torch.device): Cuda or CPU
             data_root (str, optional): Directory for Mesh, UV Table. Defaults to "/home/user/my_zjuMocap".
-        Returns: 
+        Returns:
 
         """
         super().__init__()
@@ -182,61 +219,74 @@ class TexturePoints:
         self.n_sample = n_sample
         self.current_epoch = current_epoch
         self.device = device
-        self.save_dir = join(save_path,"learned_texPts")
+        self.save_dir = join(save_path, "learned_texPts")
         self.mesh_dir = join(data_root, "50_meshes")
         self.uv_fpath = join(data_root, "uv_table.npy")
         self.data = dict()
         if not isdir(self.save_dir):
-            assert (current_epoch== -1), f"Requires Learned Texture Points for {self.current_epoch} at {self.save_dir}"
+            assert (
+                current_epoch == -1
+            ), f"Requires Learned Texture Points for {self.current_epoch} at {self.save_dir}"
             Path(self.save_dir).mkdir(parents=True, exist_ok=True)
-               
-        
-        if(self.current_epoch==-1):
+
+        if self.current_epoch == -1:
             ###############################################################
             ########    Create Initial Random 3d Texture Points    ########
             ###############################################################
             torch.manual_seed(1310)
             self.rng = default_rng(12345)
-            base_tex3d = torch.FloatTensor(self.n_sample, 3).uniform_().to(self.device) ## torch.float32, [300000, 3])
-            base_tex2d = torch.FloatTensor(self.n_sample,2).uniform_().to(self.device)
+            base_tex3d = (
+                torch.FloatTensor(self.n_sample, 3).uniform_().to(self.device)
+            )  ## torch.float32, [300000, 3])
+            base_tex2d = torch.FloatTensor(self.n_sample, 2).uniform_().to(self.device)
             # print(f"base_tex:{type(base_tex3d), base_tex3d.dtype, base_tex3d.shape}")
             # print(f"base_tex: {torch.min(base_tex3d).item(),torch.max(base_tex3d).item()}")
             person_id = self.person_ids[0]
-            mesh_fpath = join(self.mesh_dir, person_id, "%06d.obj"%self.mesh_num)         
+            mesh_fpath = join(self.mesh_dir, person_id, "%06d.obj" % self.mesh_num)
             my_mesh = trimesh.load(mesh_fpath)
             V = my_mesh.vertices
             F = my_mesh.faces
             T = np.load(self.uv_fpath)
-            
-            proj = TextureProjector3d(T,F=F)
-            pts3d, pts_uv, normal_noise, noramls = proj.random_sample(n_sample,V) ### (300000, 2) 0.01966 0.9945
+
+            proj = TextureProjector3d(T, F=F)
+            pts3d, pts_uv, normal_noise, noramls = proj.random_sample(
+                n_sample, V
+            )  ### (300000, 2) 0.01966 0.9945
             ### Convert Value Range: [0,1]->[-1,1]
-            pts_uv = 2.*(pts_uv - np.min(pts_uv))/np.ptp(pts_uv) - 1 ## (300000, 2), -1.0, 1.0)            
-            noise_fixed = self.rng.uniform(-0.1, 0.1, (self.n_sample, 1) ) ## (300000, 1)
-            uvw = np.concatenate((pts_uv, noise_fixed), axis=1) ## (300000, 3)
-            base_tex3d = torch.from_numpy(uvw.copy()).float() ## torch.float32, [300000, 3])            
+            pts_uv = (
+                2.0 * (pts_uv - np.min(pts_uv)) / np.ptp(pts_uv) - 1
+            )  ## (300000, 2), -1.0, 1.0)
+            noise_fixed = self.rng.uniform(
+                -0.1, 0.1, (self.n_sample, 1)
+            )  ## (300000, 1)
+            uvw = np.concatenate((pts_uv, noise_fixed), axis=1)  ## (300000, 3)
+            base_tex3d = torch.from_numpy(
+                uvw.copy()
+            ).float()  ## torch.float32, [300000, 3])
             # print(f"base_tex: {torch.min(base_tex3d).item(),torch.max(base_tex3d).item()}")
             base_tex2d = torch.from_numpy(pts_uv.copy()).float()
-            
-            base3d_fpath = join(self.save_dir,"base_3d.pth")
+
+            base3d_fpath = join(self.save_dir, "base_3d.pth")
             torch.save(base_tex3d, base3d_fpath)
-            base2d_fpath = join(self.save_dir,"base_2d.pth")
+            base2d_fpath = join(self.save_dir, "base_2d.pth")
             torch.save(base_tex2d, base2d_fpath)
-            for pid in self.person_ids:                
-                ##### Load the Texture Points ####              
+            for pid in self.person_ids:
+                ##### Load the Texture Points ####
                 tex_2d = torch.load(base2d_fpath).to(self.device)
                 tex_2d.requires_grad = True
                 tex_pts = torch.load(base3d_fpath).to(self.device)
                 tex_pts.requires_grad = True
-                optim = torch.optim.Adam([tex_pts, tex_2d], lr=0.05, amsgrad=True, weight_decay=0)
-                self.data[pid] = {"tex_2d":tex_2d, "tex_pts": tex_pts, "optim":optim}
+                optim = torch.optim.Adam(
+                    [tex_pts, tex_2d], lr=0.05, amsgrad=True, weight_decay=0
+                )
+                self.data[pid] = {"tex_2d": tex_2d, "tex_pts": tex_pts, "optim": optim}
 
         else:
             ###############################################################
             ########       Load Learned 3d Texture Points          ########
-            ###############################################################              
-            
-            path = join(self.save_dir, "ep%06d"%current_epoch)
+            ###############################################################
+
+            path = join(self.save_dir, "ep%06d" % current_epoch)
             for pid in self.person_ids:
                 tex2d_fpath = join(path, f"texture2d_{pid}.pth")
                 tex_fpath = join(path, f"texture3d_{pid}.pth")
@@ -248,19 +298,19 @@ class TexturePoints:
                 tex_2d.requires_grad = True
                 tex_pts = torch.load(tex_fpath).to(self.device)
                 tex_pts.requires_grad = True
-                optim = torch.optim.Adam([tex_pts], lr=0.005, 
-                                         amsgrad=True, weight_decay=0)
+                optim = torch.optim.Adam(
+                    [tex_pts], lr=0.005, amsgrad=True, weight_decay=0
+                )
                 optim_checkpoint = torch.load(optim_fpath)
                 optim.load_state_dict(optim_checkpoint["optim"])
-                self.data[pid] = {"tex_2d":tex_2d, "tex_pts": tex_pts, "optim": optim}
-    
-    
+                self.data[pid] = {"tex_2d": tex_2d, "tex_pts": tex_pts, "optim": optim}
+
     def save_learned_texPts(self, epoch=-1):
-        if (epoch > -1):
-            epoch_dir = join(self.save_dir, "ep%06d"%epoch)
+        if epoch > -1:
+            epoch_dir = join(self.save_dir, "ep%06d" % epoch)
             assert not isdir(epoch_dir), epoch_dir
             Path(epoch_dir).mkdir(parents=True, exist_ok=True)
-        
+
         for pid in self.data.keys():
             data = self.data[pid]
             tex_2d = data["tex_2d"]
@@ -273,24 +323,23 @@ class TexturePoints:
             torch.save(tex_2d, tex2d_fpath)
             torch.save(tex_pts, tex_fpath)
             # print("tex_pts is saved in :", tex_fpath)
-            
-            if(epoch > -1):
+
+            if epoch > -1:
                 tex2d_path = join(epoch_dir, f"texture2d_{pid}.pth")
                 f_path = join(epoch_dir, f"texture3d_{pid}.pth")
                 optim_path = join(epoch_dir, f"texture_{pid}_optim.pth")
-                torch.save({"optim":optim.state_dict()}, optim_path)
+                torch.save({"optim": optim.state_dict()}, optim_path)
                 torch.save(tex_pts, f_path)
                 torch.save(tex_2d, tex2d_path)
-   
-    
+
     def get_texPts_for_evaluation(self, person_ids: List[int]):
-        texPts_3d = []    
-        texPts_2d =[]
+        texPts_3d = []
+        texPts_2d = []
         for pid in person_ids:
             data = self.data[pid]
             tex_2d = data["tex_2d"]
-            tex_pts = data["tex_pts"]        
-            texPts_2d.append(tex_2d.unsqueeze(0))    
+            tex_pts = data["tex_pts"]
+            texPts_2d.append(tex_2d.unsqueeze(0))
             texPts_3d.append(tex_pts.unsqueeze(0))
         texPts_2d = torch.cat(tex_2d, dim=0)
         texPts_3d = torch.cat(texPts_3d, dim=0)
@@ -316,7 +365,7 @@ class TexturePoints:
             optim = data["optim"]
             texPts_2d.append(tex_2d.unsqueeze(0))
             texPts_3d.append(tex_pts.unsqueeze(0))
-            
+
             if pid not in already_used_pids:
                 Optim.append(optim)
                 already_used_pids.add(pid)
@@ -324,20 +373,20 @@ class TexturePoints:
         texPts_3d = torch.cat(texPts_3d, dim=0).to(self.device)
         # print(f"Texture points CUDA:{texPts_2d.is_cuda, texPts_3d.is_cuda}")
         return texPts_2d, texPts_3d, Optim
-            
-        
+
 
 if __name__ == "__main__":
     np.random.seed(1310)
-    root ="/home/user/DATA"
-    person_ids=[0]
-    mesh_num = 0 
+    root = "/home/user/DATA"
+    person_ids = [0]
+    mesh_num = 0
     n_sample = 300000
     currentEpoch = -1
-    save_path = join(root,"toDelete")
+    save_path = join(root, "toDelete")
     device = "cuda:0" if torch.cuda.is_available else "cpu"
     rng = default_rng(12345)
     noise_fixed = rng.uniform(-0.1, 0.1, (1, n_sample, 1))
     print(f"noise_fix: {noise_fixed.shape}")
-    texture_points = TexturePoints3D(person_ids, mesh_num, n_sample, currentEpoch,
-                                   save_path, device)
+    texture_points = TexturePoints3D(
+        person_ids, mesh_num, n_sample, currentEpoch, save_path, device
+    )
